905297846cd35563d3b6df076057949a86249dc3
min00000 <ahnmin410@gmail.com>
2019-03-26 17:05:08 +0900

    hello sum

---
+++ hello3.java
@@ -0,0 +1,22 @@
+package hello3;
+
+public class hello3 {
+	
+	public static int sum (int n, int m) {
+		return n+m;
+	}
+
+	public static void main(String[] args) {
+		
+		int i =20;
+		int s;
+		char a;
+		
+		s = sum(i, 10);
+		a = '?';
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
+	}
+}
+
